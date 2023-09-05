7cf01ffd1b5d2b0251107280cfe0e2fc2f015e29
Harshit01070143 <harshitsharma16971@gmail.com>
2023-09-05 08:58:43 +0530

    Add project files.

---
+++ static2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace static2
+{
+    static class Logger
+    {
+        public static void LogMessage(string message)
+        {
+            Console.WriteLine($"[LOG] {DateTime.Now}: {message}");
+            Console.ReadLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.LogMessage("Starting the application...");
+
+            // Your application logic here
+
+            Logger.LogMessage("Application finished.");
+        }
+    }
+
+
+  }
